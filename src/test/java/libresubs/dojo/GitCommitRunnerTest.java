f438ac4be8a9c719bd7c21502415583a3ced8a5a
Lucas <lcsantos@wkcb-lcsantos.(none)>
2011-05-25 18:55:34 -0300

    test do not commit on fail

--- src/test/java/libresubs/dojo/GitCommitRunnerTest.java
+++ src/test/java/libresubs/dojo/GitCommitRunnerTest.java
@@ -4,20 +4,45 @@ import junit.framework.Assert;
 import libresubs.dojo.mock.GitRepositoryMock;
 
 import org.junit.Test;
+import org.junit.runner.Description;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 
 public class GitCommitRunnerTest {
 
+	GitRepositoryMock git = new GitRepositoryMock();
+	RunListener listener = new GitCommitRunListener(git);	
+
 	@Test
 	public void testCommitOnSuccess() throws Exception{
-		GitRepositoryMock git = new GitRepositoryMock();
-		RunListener listener = new GitCommitRunListener(git);
 		Result allTestsHavePassed = new Result();
 		listener.testRunFinished(allTestsHavePassed);
 		
 		Assert.assertTrue(git.hasCommit());
+	}
+	
+	@Test
+	public void testWillNotCommitOnFailure() throws Exception{
+		Result resultWithFailedTests = createFailedResult();
+		listener.testRunFinished(resultWithFailedTests);
 		
+		Assert.assertFalse(git.hasCommit());
+		
+	}
+
+	private Result createFailedResult() throws Exception {
+		
+		Result result = new Result();
+		
+		RunListener resultListener = result.createListener();
+		
+		Description description = null;
+		Throwable throwable = null;
+		Failure failure = new Failure(description, throwable);
+		resultListener.testFailure(failure);
+		
+		return result;
 	}
 	
 }
