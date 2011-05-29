52dddf30dc32c8c10c3bd1bf26f8d4ee3a34fc99
kalecser <kalecser@gmail.com>
2011-05-29 11:58:52 -0300

    Renaming klass to testClass

--- src/main/java/libresubs/dojo/GitCommitOnSuccessRunner.java
+++ src/main/java/libresubs/dojo/GitCommitOnSuccessRunner.java
@@ -6,17 +6,17 @@ import org.junit.runners.model.InitializationError;
 
 public class GitCommitOnSuccessRunner extends BlockJUnit4ClassRunner{
 
-	private final Class<?> klass;
+	private final Class<?> testClass;
 
 	public GitCommitOnSuccessRunner(Class<?> testClass) throws InitializationError {
 		super(testClass);
-		this.klass = testClass;
+		this.testClass = testClass;
 	}
 	
 	@Override
 	public void run(RunNotifier notifier) {
 		super.run(notifier);
-		GitRepositoryForClass repository = new GitRepositoryForClass(klass);
+		GitRepositoryForClass repository = new GitRepositoryForClass(testClass);
 		GitCommitRunListener listener = new GitCommitRunListener(repository);
 		notifier.addListener(listener);
 	}
